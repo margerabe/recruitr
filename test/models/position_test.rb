5ae018ddaa61f0e5b27ef2e187322dbd625ebedf
margerabe <marsanraphael@gmail.com>
2021-11-29 17:44:28 +0100

    add a unit test and adapt environment variables for new production DB

--- test/models/position_test.rb
+++ test/models/position_test.rb
@@ -1,9 +1,27 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 class PositionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new
+    @position = Position.new(title: "The first position.", user: @user)
+  end
+
+  test 'position should be valid' do
+    assert @position.valid?
+  end
+
+  test "should not save a position without a title" do
+    position = Position.new
+    assert_not position.save, "Saved the position without a title"
+  end
+
+  test "association test - should have several comments" do
+    @position.candidates.build(first_name: "Jean-Paul", last_name: "Faucan", email: "jpfaucan@gmail.com")
+    @position.candidates.build(first_name: "Claude", last_name: "Bourvil", email: "cbourvil@gmail.com")
+    assert_equal 2, @position.candidates.size
+  end
 end
+

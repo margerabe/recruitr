db19133a902739c64938d392f239fe1d3215291b
margerabe <marsanraphael@gmail.com>
2021-11-29 22:44:31 +0100

    basic template for unit and controller tests wip

--- test/controllers/positions_controller_test.rb
+++ test/controllers/positions_controller_test.rb
@@ -3,7 +3,20 @@
 require 'test_helper'
 
 class PositionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @position = positions(:one)
+  end
+
+  test 'get index' do
+    get positions_url
+    assert_response :success
+  end
+
+  test 'create position' do
+    assert_difference('Position.count') do
+      post positions_url, params: { position: { title: @position.title } }
+    end
+
+    assert_response :created
+  end
 end
--- test/models/position_test.rb
+++ test/models/position_test.rb
@@ -3,10 +3,8 @@
 require "test_helper"
 
 class PositionTest < ActiveSupport::TestCase
-
   def setup
-    @user = User.new
-    @position = Position.new(title: "The first position.", user: @user)
+    @position = Position.new(title: "The first position.")
   end
 
   test 'position should be valid' do
@@ -18,9 +16,11 @@ class PositionTest < ActiveSupport::TestCase
     assert_not position.save, "Saved the position without a title"
   end
 
-  test "association test - should have several comments" do
-    @position.candidates.build(first_name: "Jean-Paul", last_name: "Faucan", email: "jpfaucan@gmail.com")
-    @position.candidates.build(first_name: "Claude", last_name: "Bourvil", email: "cbourvil@gmail.com")
+  test "association test - should have several candidates" do
+    candidate1 = @position.candidates.build(first_name: "Jean-Paul", last_name: "Faucan", email: "jpfaucan@gmail.com")
+    candidate1.save
+    candidate2 = @position.candidates.build(first_name: "Claude", last_name: "Bourvil", email: "cbourvil@gmail.com")
+    candidate2.save
     assert_equal 2, @position.candidates.size
   end
 end

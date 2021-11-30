b508e8d69fcea856cb20c28c6130f8bd3cd09340
margerabe <marsanraphael@gmail.com>
2021-11-30 18:15:24 +0100

    recruiter controller tests

---
+++ test/controllers/recruiters_controller.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class RecruitersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @recruiter = recruiters(:one)
+  end
+
+  test 'INDEX recruiter' do
+    get recruiters_url
+
+    assert_response :success
+  end
+
+  test 'SHOW recruiter' do
+    get recruiter_url(@recruiter)
+
+    assert_response :success
+  end
+
+  test 'CREATE recruiter' do
+    assert_difference('Candidate.count') do
+      post recruiters_url, params: { recruiter: 
+                                      { 
+                                        first_name: "François",
+                                        last_name: "Dupont",
+                                        email: "fdupont@gmail.com"
+                                      }
+                                    }
+    end
+
+    assert_response :created
+  end
+
+  test 'cannot CREATE recruiter with existing email' do
+    post recruiters_url, params: { recruiter: 
+                                    { 
+                                      first_name: "François",
+                                      last_name: "Dupont",
+                                      email: @recruiter.email
+                                    }
+                                  }
+
+    assert_response :unprocessable_entity
+  end
+
+  test 'UPDATE recruiter' do
+    patch recruiter_url(@recruiter), params: { recruiter: { first_name: "Bertrand" } }
+    json_response = JSON.parse(response.body)
+
+    assert_response :success
+    assert_equal "Bertrand", json_response["first_name"]
+  end
+
+  test 'should not UPDATE recruiter with blank email' do
+    patch recruiter_url(@recruiter), params: { recruiter: { email: "" } }
+
+    assert_response :unprocessable_entity
+  end
+
+  test 'DESTROY recruiter' do
+    assert_difference('Candidate.count', -1) do
+      delete recruiter_url(@recruiter)
+    end
+
+    assert_response :no_content
+  end
+end

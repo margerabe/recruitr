5fd7de5213b8186652908d16f6dc502f9b74a72a
margerabe <marsanraphael@gmail.com>
2021-11-30 20:43:31 +0100

    interview basic unit tests

--- app/serializers/interview_serializer.rb
+++ app/serializers/interview_serializer.rb
@@ -14,6 +14,6 @@ class InterviewSerializer < ActiveModel::Serializer
   end
 
   def position
-    object.candidate.position
+    object.candidate.position.title
   end
 end
---
+++ test/models/interview_test.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class InterviewTest < ActiveSupport::TestCase
+  def setup
+    @interview = interviews(:one)
+    @candidate = candidates(:two)
+    @recruiter = recruiters(:two)
+  end
+
+  test 'interview should be valid' do
+    new_interview = Interview.new(
+      start_time: DateTime.new(2022,10,13,10,0,0),
+      end_time: DateTime.new(2022,10,13,11,0,0),
+      candidate: candidates(:two),
+      recruiter: recruiters(:two)
+    )
+    assert new_interview.valid?
+  end
+
+  test 'interview should start and end on the same day' do
+    new_interview = Interview.new(
+      start_time: DateTime.new(2022,10,13,10,0,0),
+      end_time: DateTime.new(2022,10,14,11,0,0),
+      candidate: candidates(:two),
+      recruiter: recruiters(:two)
+    )
+    assert_not new_interview.valid?
+  end
+
+  test 'interview should be scheduled between 10:00 and 18:00' do
+    new_interview = Interview.new(
+      start_time: DateTime.new(2022,10,13,9,0,0),
+      end_time: DateTime.new(2022,10,13,10,0,0),
+      candidate: candidates(:two),
+      recruiter: recruiters(:two)
+    )
+    assert_not new_interview.valid?
+  end
+end
+

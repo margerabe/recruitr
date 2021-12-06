f3ee9a4c4248a7dfee4921f8a4c6b8ace711a712
margerabe <marsanraphael@gmail.com>
2021-12-06 11:37:25 +0100

    adds authenticable module for managing authentication methods (fetching current_user) from JWT token passed in headers of request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include Authenticable
+  
   def render_error(object)
     render json: { errors: object.errors.full_messages }, status: :unprocessable_entity
   end

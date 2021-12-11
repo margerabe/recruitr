951491069cd095cd772c6d952ac626f0dcfebb19
margerabe <marsanraphael@gmail.com>
2021-12-11 15:11:11 +0100

    first passing tests

---
+++ spec/factories/positions.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :position do
+    title { "Full Stack Developer" }
+  end
+end
--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Position, type: :model do
-  true
+  before(:all) do
+    @position1 = create(:position)
+  end
+  
+  it "is valid with valid attributes" do
+    expect(@position1).to be_valid
+  end
+
+  it "title cannot be blank" do
+    position2 = build(:position, title: nil)
+    expect(position2).to_not be_valid
+  end
 end
